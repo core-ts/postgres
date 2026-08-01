10ee0fb1f6a4602e9f99c077c54fdf8bb68d3db3
Duc Nguyen <minhduc140583@gmail.com>
2026-08-02 06:06:11 +0700

    Refactor code

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,70 +1,50 @@
 export interface StringMap {
-  [key: string]: string
+  [key: string]: string;
 }
 export interface Statement {
-  query: string
-  params?: any[]
+  query: string;
+  params?: any[];
 }
 
 export interface Executor {
-  driver: string
-  param(i: number): string
-  execute(sql: string, args?: any[], ctx?: any): Promise<number>
-  executeBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>
-  query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>
-  queryOne<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T | null>
-  executeScalar<T>(sql: string, args?: any[], ctx?: any): Promise<T>
-  count(sql: string, args?: any[], ctx?: any): Promise<number>
+  driver: string;
+  param(i: number): string;
+  execute(sql: string, args?: any[], ctx?: any): Promise<number>;
+  executeBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>;
+  query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>;
+  queryOne<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T | null>;
+  executeScalar<T>(sql: string, args?: any[], ctx?: any): Promise<T>;
+  count(sql: string, args?: any[], ctx?: any): Promise<number>;
 }
 export interface Transaction extends Executor {
-  commit(): Promise<void>
-  rollback(): Promise<void>
+  commit(): Promise<void>;
+  rollback(): Promise<void>;
 }
 export interface DB extends Executor {
-  beginTransaction(): Promise<Transaction>
+  beginTransaction(): Promise<Transaction>;
 }
 export interface MinDB {
-  driver: string
-  param(i: number): string
-  execute(sql: string, args?: any[], ctx?: any): Promise<number>
-  executeBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>
-  query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>
+  driver: string;
+  param(i: number): string;
+  execute(sql: string, args?: any[], ctx?: any): Promise<number>;
+  executeBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>;
+  query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>;
 }
-export type DataType =
-  | "ObjectId"
-  | "date"
-  | "datetime"
-  | "time"
-  | "boolean"
-  | "number"
-  | "integer"
-  | "string"
-  | "text"
-  | "object"
-  | "array"
-  | "binary"
-  | "primitives"
-  | "booleans"
-  | "numbers"
-  | "integers"
-  | "strings"
-  | "dates"
-  | "datetimes"
-  | "times"
+export type DataType = "ObjectId" | "date" | "datetime" | "time" | "boolean" | "number" | "integer" | "string" | "text" | "object" | "array" | "binary" | "primitives" | "booleans" | "numbers" | "integers" | "strings" | "dates" | "datetimes" | "times";
 
 export interface Attribute {
-  name?: string
-  column?: string
-  type?: DataType
-  default?: string | number | Date | boolean
-  key?: boolean
-  noinsert?: boolean
-  noupdate?: boolean
-  version?: boolean
-  ignored?: boolean
-  true?: string | number
-  false?: string | number
+  name?: string;
+  column?: string;
+  type?: DataType;
+  default?: string | number | Date | boolean;
+  key?: boolean;
+  noinsert?: boolean;
+  noupdate?: boolean;
+  version?: boolean;
+  ignored?: boolean;
+  true?: string | number;
+  false?: string | number;
 }
 export interface Attributes {
-  [key: string]: Attribute
+  [key: string]: Attribute;
 }
